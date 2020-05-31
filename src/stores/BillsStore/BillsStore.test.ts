db8de275a1c4d260dbd6742486292178a7d08329
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-31 19:46:43 +0100

    test: covers fetch failure case

--- src/stores/BillsStore/BillsStore.test.ts
+++ src/stores/BillsStore/BillsStore.test.ts
@@ -15,12 +15,40 @@ describe('BillsStore tests', () => {
     expect(billsStore.fetching).toBe(false)
     expect(billsStore.finished).toBe(false)
     expect(billsStore.errorMessage).toEqual('')
+
     const fetchPromise = billsStore.fetch()
+
     expect(billsStore.fetching).toBe(true)
     expect(billsStore.finished).toBe(false)
+
     await fetchPromise
+
     expect(billsStore.fetching).toBe(false)
     expect(billsStore.finished).toBe(true)
     expect(billsStore.bills).toHaveLength(9)
+  })
+
+  it('handles http errors and stores the response', async () => {
+    const mock = nock('http://localhost').persist().get('/bills').reply(500)
+
+    const billsStore = new BillsStore('http://localhost/bills')
+    expect(billsStore.bills).toHaveLength(0)
+    expect(billsStore.fetching).toBe(false)
+    expect(billsStore.finished).toBe(false)
+    expect(billsStore.errorMessage).toEqual('')
+
+    const fetchPromise = billsStore.fetch()
+
+    expect(billsStore.fetching).toBe(true)
+    expect(billsStore.finished).toBe(false)
+
+    await fetchPromise
+
+    expect(billsStore.fetching).toBe(false)
+    expect(billsStore.finished).toBe(false)
+    expect(billsStore.bills).toHaveLength(0)
+    expect(billsStore.errorMessage).toEqual('Internal Server Error')
+
+    mock.persist(false)
   })
 })
