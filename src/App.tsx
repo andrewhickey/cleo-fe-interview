b88f731474985c94978a578608183f76eb2a49d9
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-31 20:41:50 +0100

    chore: configures tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Tabs } from './components'
 import { StoreProvider } from './stores'
+import './tailwind.generated.css'
 
 class App extends Component {
   render() {
