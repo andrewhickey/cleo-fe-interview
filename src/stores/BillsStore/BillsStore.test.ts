38db0e8738b250d74140233b15ade976e3f2d145
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-31 19:22:04 +0100

    feat: adds ability to fetch bills from the api

--- src/stores/BillsStore/BillsStore.test.ts
+++ src/stores/BillsStore/BillsStore.test.ts
@@ -2,6 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import BillsStore from './BillsStore'
 
-it('can be instantiated', () => {
-  new BillsStore()
+describe('BillsStore tests', () => {
+  it('can be instantiated', () => {
+    new BillsStore()
+  })
+
+  it('fetches bills and stores them in state', async () => {
+    const billsStore = new BillsStore()
+    expect(billsStore.bills).toHaveLength(0)
+    expect(billsStore.fetching).toBe(false)
+    expect(billsStore.finished).toBe(false)
+    expect(billsStore.errorMessage).toEqual('')
+    const fetchPromise = billsStore.fetch()
+    expect(billsStore.fetching).toBe(true)
+    expect(billsStore.finished).toBe(false)
+    await fetchPromise
+    expect(billsStore.fetching).toBe(false)
+    expect(billsStore.finished).toBe(true)
+    expect(billsStore.bills).toHaveLength(9)
+  })
 })
--- src/stores/BillsStore/BillsStore.ts
+++ src/stores/BillsStore/BillsStore.ts
@@ -1,9 +1,45 @@
+import ky from 'ky/umd'
 import { observable } from 'mobx'
 
+type Transaction = {
+  amount: number
+  date: string
+  id: number
+}
+
+type Bill = {
+  categoryId: number
+  iconUrl: string
+  id: string
+  isBill: boolean
+  name: string
+  transactions: Transaction[]
+}
+
 class BillsStore {
-  id = Math.random()
-  @observable title = ''
+  endpoint: string
+
+  @observable fetching = false
   @observable finished = false
+  @observable errorMessage = ''
+  @observable bills: Bill[] = []
+
+  constructor(endpoint = 'http://localhost:3002/bills') {
+    this.endpoint = endpoint
+  }
+
+  fetch = async () => {
+    try {
+      this.fetching = true
+      const bills = await ky.get(this.endpoint).json()
+      this.bills = bills as Bill[]
+      this.finished = true
+    } catch (error) {
+      this.errorMessage = error.message
+    }
+
+    this.fetching = false
+  }
 }
 
 export default BillsStore
